03a6559706e88171d56802272ccd66c9d1d14685
Daniel Kennett <github@ikennd.ac>
2014-08-10 12:57:05 +0200

    Add license

--- main.m
+++ main.m
@@ -3,8 +3,7 @@
 //  generate-string-symbols
 //
 //  Created by Daniel Kennett on 07/08/14.
-//  Copyright (c) 2014 Daniel Kennett. All rights reserved.
-//
+//  For license information, see LICENSE.markdown
 
 #import <Foundation/Foundation.h>
 
